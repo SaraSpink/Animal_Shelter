43c5b918f7de05b2509d4aeeb7a9e8fda8a3700f
Karla Buckner and Sara Spink <karla@>
2017-09-19 16:37:57 -0700

    Add one more integration spec and pass

--- spec/animal_shelter_integrated_spec.rb
+++ spec/animal_shelter_integrated_spec.rb
@@ -4,11 +4,21 @@ Capybara.app = Sinatra::Application
 set(:Show_exception, false)
 
 describe('adding a new pet', {:type => :feature}) do
-  it('allows a user to click a list to see the tasks and details for it') do
+  it('allows a user to click a list to see the pets and details for it') do
     visit('/')
     click_link('View all adoptable animals')
     fill_in('name', :with => 'Dixie')
     click_button('Add this pet!')
     expect(page).to have_content('Dixie')
+  end
+end
+
+describe('adding a new owner', {:type => :feature}) do
+  it('allows a user to click a list to see the owner and details for it') do
+    visit('/')
+    click_link('View all potential owners')
+    fill_in('name', :with => 'Karla')
+    click_button('Add this potential owner!')
+    expect(page).to have_content('Karla')
   end
 end
